1b1f9f4955af49adddfdde366cf5622f5a627b95
Francisco Edno <francisco.rodrigues@arquivei.com.br>
2022-03-22 19:34:06 -0300

    Publish messages in test

---
+++ tests/System/CreateInvoiceTest.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\System;
+
+use Tests\TestCase;
+
+class CreateInvoiceTest extends TestCase
+{
+    public function testShouldCreateInvoiceInSAP(): void
+    {
+        $data = [
+            'value' => 4200.42,
+        ];
+
+        $topicName = 'com.arquivei.events.nfse-was-emitted' . uniqid();
+
+        // Enviar um evento
+        $rdKafkaConf = new \RdKafka\Conf();
+        $rdKafkaConf->set('security.protocol', 'PLAINTEXT');
+        $rdKafkaConf->set('sasl.mechanisms', 'PLAIN');
+        $rdKafkaConf->set('bootstrap.servers', 'kafka:9092');
+
+        $producer = new \RdKafka\Producer($rdKafkaConf);
+
+        $topic = $producer->newTopic($topicName);
+        $topic->produce(RD_KAFKA_PARTITION_UA, 0, json_encode($data));
+
+        $producer->flush(12000);
+    }
+}
